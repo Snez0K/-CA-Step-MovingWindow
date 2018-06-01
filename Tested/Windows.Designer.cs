79746634479e6f00a399755a518cf963670ba6a4
Snez0 <Snez0@DESKTOP-RON656Q>
2018-06-01 22:50:57 +0300

    Add project files.

---
+++ Tested/Windows.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Windows
+{
+    partial class Windows
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Windows
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Windows";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Windows_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Tested/Windows.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Windows
+{
+    public partial class Windows : Form
+    {
+        private int move = -1;
+        private int checkX = 6;
+        private int checkY = 6;
+
+        public Windows()
+        {
+            InitializeComponent();
+        }
+
+        private void Windows_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                move = 0; 
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                move = 1;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                move = 2;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                move = 3;
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                move = -1;
+                Location = new Point((Screen.PrimaryScreen.Bounds.Size.Width / 2) - (Size.Width / 2), (Screen.PrimaryScreen.Bounds.Size.Height / 2) - (Size.Height / 2));
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (move == 0)
+            {
+                if (Location.X < 6)
+                {
+                    Location = new Point(0, Location.Y);
+                    move = 2;
+                }
+                else
+                {
+                    Location = new Point(Location.X - checkX, Location.Y);
+                }
+            }else if (move == 1)
+            {
+                if (Location.Y < 6)
+                {
+                    Location = new Point(Location.X, 0);
+                    move = 3;
+                }
+                else
+                {
+                    Location = new Point(Location.X, Location.Y - checkY);
+                }
+            }
+            else if (move == 2)
+            {
+                if (Location.X > Screen.PrimaryScreen.Bounds.Size.Width - (ClientSize.Width + 22))
+                {
+                    Location = new Point(Screen.PrimaryScreen.Bounds.Size.Width - (ClientSize.Width + 16), Location.Y);
+                    move = 0;
+                }
+                else
+                {
+                    Location = new Point(Location.X + checkX, Location.Y);
+                } 
+            }else if (move == 3)
+            {
+                if (Location.Y > Screen.PrimaryScreen.Bounds.Size.Height - (ClientSize.Height + 45))
+                {
+                    Location = new Point(Location.X, Screen.PrimaryScreen.Bounds.Size.Height - (ClientSize.Height + 39));
+                    move = 1;
+                }
+                else
+                {
+                    Location = new Point(Location.X, Location.Y + checkY);
+                } 
+            }
+        }
+    }
+}
