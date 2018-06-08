6813baf2aac76bac990177c6dc17305df25b78cc
Snez0 <Snez0@DESKTOP-RON656Q>
2018-06-08 19:57:48 +0300

    Fixed #7

--- MovingWindow/Window.cs
+++ MovingWindow/Window.cs
@@ -6,9 +6,18 @@ namespace Windows
 {
     public partial class Window : Form
     {
-        private int move = -1;
-        private int checkX = 6;
-        private int checkY = 6;
+        private const int stay = -1;
+        private const int left = 0;
+        private const int up = 1;
+        private const int right = 2;
+        private const int down = 3;
+        private int status = -1; 
+        private const int checkX = 6; //Дальность шага у окна по оси Х
+        private const int checkY = 6; //Дальность шага у окна по оси У
+        private const int widthFirst = 16;
+        private const int widthSecond = 22;
+        private const int heightFirst = 39;
+        private const int heightSecond = 45;
 
         public Window()
         {
@@ -19,71 +28,71 @@ namespace Windows
         {
             if (e.KeyCode == Keys.Left)
             {
-                move = 0; 
+                status = left; 
             }
             else if (e.KeyCode == Keys.Up)
             {
-                move = 1;
+                status = up;
             }
             else if (e.KeyCode == Keys.Right)
             {
-                move = 2;
+                status = right; 
             }
             else if (e.KeyCode == Keys.Down)
             {
-                move = 3;
+                status = down; 
             }
             else if (e.KeyCode == Keys.Enter)
             {
-                move = -1;
+                status = stay; 
                 Location = new Point((Screen.PrimaryScreen.Bounds.Size.Width / 2) - (Size.Width / 2), (Screen.PrimaryScreen.Bounds.Size.Height / 2) - (Size.Height / 2));
             }
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            if (move == 0)
+            if (status == left)
             {
-                if (Location.X < 6)
+                if (Location.X < checkX)
                 {
                     Location = new Point(0, Location.Y);
-                    move = 2;
+                    status = right;
                 }
                 else
                 {
                     Location = new Point(Location.X - checkX, Location.Y);
                 }
             }
-            else if (move == 1)
+            else if (status == up)
             {
-                if (Location.Y < 6)
+                if (Location.Y < checkY)
                 {
                     Location = new Point(Location.X, 0);
-                    move = 3;
+                    status = down;
                 }
                 else
                 {
                     Location = new Point(Location.X, Location.Y - checkY);
                 }
             }
-            else if (move == 2)
+            else if (status == right)
             {
-                if (Location.X > Screen.PrimaryScreen.Bounds.Size.Width - (ClientSize.Width + 22))
+                if (Location.X > Screen.PrimaryScreen.Bounds.Size.Width - (ClientSize.Width + widthSecond))
                 {
-                    Location = new Point(Screen.PrimaryScreen.Bounds.Size.Width - (ClientSize.Width + 16), Location.Y);
-                    move = 0;
+                    Location = new Point(Screen.PrimaryScreen.Bounds.Size.Width - (ClientSize.Width + widthFirst), Location.Y);
+                    status = left;
                 }
                 else
                 {
                     Location = new Point(Location.X + checkX, Location.Y);
                 } 
             }
-            else if (move == 3)
+            else if (status == down)
             {
-                if (Location.Y > Screen.PrimaryScreen.Bounds.Size.Height - (ClientSize.Height + 45))
+                if (Location.Y > Screen.PrimaryScreen.Bounds.Size.Height - (ClientSize.Height + heightSecond))
                 {
-                    Location = new Point(Location.X, Screen.PrimaryScreen.Bounds.Size.Height - (ClientSize.Height + 39));
-                    move = 1;
+                    Location = new Point(Location.X, Screen.PrimaryScreen.Bounds.Size.Height - (ClientSize.Height + heightFirst));
+                    status = up;
                 }
                 else
                 {
